4656f67170df47a853510d0404a1acccf7126566
WilsonCazarre <wcs0486@gmail.com>
2021-04-05 22:16:58 -0300

    New commands to manage server

--- bot.py
+++ bot.py
@@ -1,16 +1,34 @@
 import os
+import subprocess
 
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
+memory = "4096M"
+minecraft_executable = ""
+minecraft_command = f'sudo java -Xmx{memory} -Xms{memory} -jar {minecraft_executable} nogui'
+process: subprocess.Popen = None
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f"Cheers Love, the {self.user} is here!")
+bot = commands.Bot(command_prefix='$')
 
 
-client = MyClient()
-client.run(os.getenv('TOKEN'))
+@bot.command()
+async def start(ctx):
+    process = subprocess.Popen(minecraft_command)
+    ctx.send('Server started')
+
+
+@bot.command()
+async def stop(ctx):
+    if process:
+        ctx.send('Server stopped')
+
+
+@bot.command()
+async def log(ctx):
+    if process:
+        ctx.send(process.stdout)
+
+bot.run(os.getenv('TOKEN'))
