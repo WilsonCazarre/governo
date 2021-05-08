e10eface2e2970e01471a4f5ef2ce04bc696229c
WilsonCazarre <wcs0486@gmail.com>
2021-05-08 00:36:25 -0300

    feat: format code with black

--- bot.py
+++ bot.py
@@ -15,21 +15,21 @@ load_dotenv()
 memory = "4096M"
 
 server: Server = Server(memory)
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix="$")
 
 
-@bot.command(name='list')
+@bot.command(name="list")
 async def list_servers(ctx: commands.Context):
     """
     Lists all the available servers.
     """
     embed = discord.Embed(
         title="Minecraft versions",
-        description="This are the available servers to run"
+        description="This are the available servers to run",
     )
     servers = server.discover_paths()
     for s in range(len(servers)):
-        embed.add_field(name=servers[s], value=f'ID: {s + 1}', inline=False)
+        embed.add_field(name=servers[s], value=f"ID: {s + 1}", inline=False)
     await ctx.send(embed=embed)
 
 
@@ -39,10 +39,10 @@ async def stop_server(ctx: commands.Context):
     Stops the current running server.
     """
     if server.process.poll() is None:
-        await ctx.send('Shutting down internal server...')
+        await ctx.send("Shutting down internal server...")
         server.stop()
         await bot.change_presence(status=discord.Status.idle)
-        await ctx.send('Server was stopped')
+        await ctx.send("Server was stopped")
     else:
         await ctx.send("There's no server running...")
 
@@ -58,20 +58,24 @@ async def run_server(ctx: commands.Context, server_id: int):
     server.stop()
     server.run(server_id - 1)
     server_name = server.paths[server_id - 1].name
-    await bot.change_presence(activity=discord.Game(
-        name=f"Hosting {server_name}")
+    await bot.change_presence(
+        activity=discord.Game(name=f"Hosting {server_name}")
     )
-    await ctx.send(embed=discord.Embed(
-        title=f"{server_name} is yours",
-        description=f"Your server is currently running and can be accessed "
-                    f"**{SERVER_HOST_NAME}**"
-    ))
+    await ctx.send(
+        embed=discord.Embed(
+            title=f"{server_name} is yours",
+            description=f"Your server is currently running and can be accessed "
+            f"**{SERVER_HOST_NAME}**",
+        )
+    )
 
 
 @run_server.error
-async def on_run_server_error(ctx: commands.Context, error: errors.CommandError):
+async def on_run_server_error(
+    ctx: commands.Context, error: errors.CommandError
+):
     if isinstance(error, errors.MissingRequiredArgument):
-        await ctx.send('Você precisa especificar um ID para o servidor.')
+        await ctx.send("Você precisa especificar um ID para o servidor.")
     else:
         raise error
 
@@ -82,38 +86,40 @@ async def log_server(ctx: commands.Context):
     Returns the java process log of the running server.
     """
     if server.process.poll() is None:
-        with open('server_log.txt', 'r') as log_file:
-            message = ''
+        with open("server_log.txt", "r") as log_file:
+            message = ""
             for line in log_file.readlines():
                 if len(message) + len(line) >= DISCORD_MAX_BODY_LENGTH:
                     await ctx.send(f"```{message}```")
-                    message = ''
+                    message = ""
                 message += line
 
             if len(message) == 0:
-                await ctx.send('Nothing to log yet')
+                await ctx.send("Nothing to log yet")
             else:
                 await ctx.send(f"```{message}```")
     else:
-        await ctx.send(embed=discord.Embed(
-            title="No server running",
-            description=f"You can start one by running {ctx.prefix}run_server <server_id>"
-        ))
+        await ctx.send(
+            embed=discord.Embed(
+                title="No server running",
+                description=f"You can start one by running {ctx.prefix}run_server <server_id>",
+            )
+        )
 
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(
-        status=discord.Status.idle
-    )
-    print(f'Cheers love, the {bot.user} is here!')
+    await bot.change_presence(status=discord.Status.idle)
+    print(f"Cheers love, the {bot.user} is here!")
 
 
 @bot.event
 async def on_command_error(ctx: commands.Context, error: errors.CommandError):
     if isinstance(error, errors.CommandNotFound):
-        await ctx.send("Looks like you're a little lost. "
-                       "Try $help to see all the available commands.")
+        await ctx.send(
+            "Looks like you're a little lost. "
+            "Try $help to see all the available commands."
+        )
     else:
         raise error
 
@@ -127,4 +133,4 @@ def goodbye():
 
 bot.add_cog(Help(bot))
 
-bot.run(get_env_variable('TOKEN'))
+bot.run(get_env_variable("TOKEN"))
--- server.py
+++ server.py
@@ -10,34 +10,38 @@ class Server:
         self.memory = memory
         self.java_executable = get_env_variable("JAVA_EXECUTABLE")
         self.process: Optional[subprocess.Popen] = subprocess.Popen(
-            ['dir'], shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE
+            ["dir"], shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE
         )
-        self.minecraft_executable = 'server.jar'
-        self.server_name = ''
+        self.minecraft_executable = "server.jar"
+        self.server_name = ""
         self.paths = []
         self.discover_paths()
 
     def run(self, version_idx):
         if self.process.poll() == 0:
             w_dir = self.paths[version_idx]
-            command = [self.java_executable,
-                       f'-Xmx{self.memory}',
-                       f'-Xms{self.memory}',
-                       '-jar',
-                       f'{w_dir}/{self.minecraft_executable}',
-                       'nogui']
-            with open('server_log.txt', 'w') as log_file:
-                self.process = subprocess.Popen(command,
-                                                stdout=log_file,
-                                                stderr=log_file,
-                                                stdin=subprocess.PIPE,
-                                                text=True,
-                                                universal_newlines=True,
-                                                cwd=w_dir)
-                print(f'Started new process: {self.process}')
+            command = [
+                self.java_executable,
+                f"-Xmx{self.memory}",
+                f"-Xms{self.memory}",
+                "-jar",
+                f"{w_dir}/{self.minecraft_executable}",
+                "nogui",
+            ]
+            with open("server_log.txt", "w") as log_file:
+                self.process = subprocess.Popen(
+                    command,
+                    stdout=log_file,
+                    stderr=log_file,
+                    stdin=subprocess.PIPE,
+                    text=True,
+                    universal_newlines=True,
+                    cwd=w_dir,
+                )
+                print(f"Started new process: {self.process}")
 
     def discover_paths(self):
-        versions_folder = BASE_DIR / 'versions'
+        versions_folder = BASE_DIR / "versions"
         new_paths = [
             p.resolve() for p in versions_folder.iterdir() if p.is_dir()
         ]
@@ -46,11 +50,11 @@ class Server:
 
     def stop(self):
         if self.process.poll() is None:
-            self.process.communicate('/stop\n')
-            print(f'Stopping process: {self.process}')
+            self.process.communicate("/stop\n")
+            print(f"Stopping process: {self.process}")
 
 
-if __name__ == '__main__':
-    server = Server('1024M')
+if __name__ == "__main__":
+    server = Server("1024M")
     server.discover_paths()
     server.run(0)
